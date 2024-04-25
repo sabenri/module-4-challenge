8ebf75838b5167dbd04ae484e596bc434b910214
sabenri <enriquezsabely@gmail.com>
2024-04-25 18:48:37 -0400

    added some java and emojis

---
+++ assets/js/blog.js
@@ -0,0 +1,9 @@
+const UserName = document.querySelector ('#username');
+const BlogName = document.querySelector('#title');
+const ContentText = docment.querySelector('#content');
+const SubmitButton = socument.querySelector('#button'):
+
+let UserNameArray = [];
+
+SubmitButton.addEventListener('click', function (event) );
+}
