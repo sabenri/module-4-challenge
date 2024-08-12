193b8cb95b40435053478e993335eec9621bb56e
Sabely Sabely <enriquezsabely@gmail.com>
2024-08-12 19:50:12 -0400

    added some java and changed a button

---
+++ assets/js/form.js
@@ -0,0 +1,4 @@
+const Username = document.querySelector('#username');
+const BlogTitle = document.querySelector('#Title');
+const BolgContent = document.querySelector('#Content');
+const button = document.querySelector('#submit');
